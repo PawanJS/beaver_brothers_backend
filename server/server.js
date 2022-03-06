13cbb5e67a8ef74dae1b9a29377ffb7249b19d4a
PawanJS <contactpawanjs@gmail.com>
2022-03-06 23:30:15 +0530

    Added backend for beaver brothers

--- server/server.js
+++ server/server.js
@@ -5,17 +5,7 @@ const router = express.Router();
 const nodemailer = require('nodemailer');
 const app = express();
 
-app.use(function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Credentials', true);
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-  res.header(
-    'Access-Control-Allow-Headers',
-    'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json'
-  );
-  next();
-});
-
+app.use(cors());
 app.use(express.json());
 app.use('/', router);
 app.listen(3002);
