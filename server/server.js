39c9fc2b744b378dc11a4e649863f06d4729aee0
PawanJS <contactpawanjs@gmail.com>
2022-03-07 21:40:18 +0530

    Added backend for beaver brothers

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use('/', router);
-app.listen(3002);
+app.listen(process.env.PORT || 3002);
 
 const transport = {
   host: 'smtp.gmail.com',
